76f769314887c18c8fccebdddd1e95071b206c34
jcitysinner <jessesinfield@Jesses-MacBook-Pro.local>
2014-12-05 23:29:25 -0500

    Portfolio Page Built

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,10 +10,11 @@ jQuery(function($) {
   // -----------------------------
 
   var portfolio = [
-    {name: 'Jobcognito', image: '../assets/images/jobcognito.png', link: 'http://jessesinfield.com/job/'},
-    {name: 'Save The Game', image: '../assets/images/savethegame.png', link: 'http://jessesinfield.com/save/'},
-    {name: 'That One Gaming Channel', image: '../assets/images/thatonegamechannel.png', link: 'http://jessesinfield.com/chan/'},
-    {name: 'Not Far From The Tree', image: '../assets/images/notfarfromthetree.png', link: 'http://www.jessesinfield.com/notFarFromTheTree/'},
+    {name: 'Jobcognito', image: '../assets/images/jobcognito.png', link: 'http://jessesinfield.com/job/', desc: 'Jobcognito is a social media platform that leverages its user&amp;s personal LinkedIn profile to allow them to create their own personal and private circles in which they can share a private job status, hidden to the prying eyes of the world.'},
+    {name: 'Save The Game', image: '../assets/images/savethegame.png', link: 'http://jessesinfield.com/save/', desc: 'Save The Game is an experiment I built utilizing the Best Buy API. It takes the ten newest games on sale at best buy (for each console) and decides how good of a deal it is. A great tool for Black Friday and Boxing Day!'},
+    {name: 'That One Gaming Channel', image: '../assets/images/thatonegamechannel.png', link: 'http://jessesinfield.com/chan/', desc:'That One Game Channel is a youtube Let&amp;s Play channel run by me and my brother Jake. We needed a website, so I took an afternoon and hashed this site together with some sleek HTML and CSS as well as some great social media APIs.'},
+    {name: 'Not Far From The Tree', image: '../assets/images/notfarfromthetree.png', link: 'http://www.jessesinfield.com/notFarFromTheTree/', desc:'Not Far from the Tree is a local Toronto charity focused on picking apples and getting those apples to the right people. Along with the help of brilliant designer Will Du, we built this site out as re-design of their original website.'},
+    {name: 'Em Kwissa, Poet', image:'../assets/images/emkwissa.png', link:'http://www.thekwissa.com/', desc:'Em Kwissa&amp;s website was the first ever freelance website I built, fresh out of my first year at Humber College. This site is built on a wordpress backend and is by far my classiest looking website!'}
   ]
 
   // -----------------------------
@@ -218,7 +219,13 @@ jQuery(function($) {
 
       // Some page data
       this.model.set({
-        content: '<h1>Portfolio Page</h1>'
+        portfolioItems: [
+          {name: portfolio[0].name, image: portfolio[0].image, link: portfolio[0].link, desc: portfolio[0].desc},
+          {name: portfolio[1].name, image: portfolio[1].image, link: portfolio[1].link, desc: portfolio[1].desc},
+          {name: portfolio[2].name, image: portfolio[2].image, link: portfolio[2].link, desc: portfolio[2].desc},
+          {name: portfolio[3].name, image: portfolio[3].image, link: portfolio[3].link, desc: portfolio[3].desc},
+          {name: portfolio[4].name, image: portfolio[4].image, link: portfolio[4].link, desc: portfolio[4].desc}
+        ]
       });
 
     },
